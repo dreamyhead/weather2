e07bcbc795d2eee78b9a3722e3d402746cfa25fc
Alex <dr3amyh3ad@gmail.com>
2021-06-23 23:04:58 +0300

    modifications: added exception handling

--- weather2/weather2/MainPage.xaml.cs
+++ weather2/weather2/MainPage.xaml.cs
@@ -21,43 +21,58 @@ namespace weather2
 
         private void OnButtonClicked(object sender, EventArgs e)
         {
-            string city = entry1.Text.Trim(' ');
-            
-            Request(city);
+            try
+            {
+                string city = entry1.Text.Trim(' ');
+                
+                Request(city);
+            }
+            catch (NullReferenceException)
+            {
+                entry1.Placeholder = "Введите город";
+            }
         }
 
         public async void Request(string city)
         {
-            await Task.Run(() =>
+            try
             {
-                string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=4f592b2566721ec99b69bb95df24da9a";
-
-                HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
-                HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
-
-                string response;
-
-                using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                await Task.Run(() =>
                 {
-                    response = streamReader.ReadToEnd();
-                }
+                    string url = $"https://api.openweathermap.org/data/2.5/weather?q={city}&units=metric&appid=4f592b2566721ec99b69bb95df24da9a";
 
-                WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
+                    HttpWebRequest httpWebRequest = (HttpWebRequest)WebRequest.Create(url);
+                    HttpWebResponse httpWebResponse = (HttpWebResponse)httpWebRequest.GetResponse();
+
+                    string response;
+
+                    using (StreamReader streamReader = new StreamReader(httpWebResponse.GetResponseStream()))
+                    {
+                        response = streamReader.ReadToEnd();
+                    }
+
+                    WeatherResponse weatherResponse = JsonConvert.DeserializeObject<WeatherResponse>(response);
 
 
-                Device.BeginInvokeOnMainThread(() =>
-                {
-                    if (weatherResponse.Main.Temp >= 0)
-                        labelDegree.Text = $"+{Math.Round(weatherResponse.Main.Temp, 1)}°C";
-                    else
-                        labelDegree.Text = $"{Math.Round(weatherResponse.Main.Temp, 1)}°C";
+                    Device.BeginInvokeOnMainThread(() =>
+                    {
+                        if (weatherResponse.Main.Temp >= 0)
+                            labelDegree.Text = $"+{Math.Round(weatherResponse.Main.Temp, 1)}°C";
+                        else
+                            labelDegree.Text = $"{Math.Round(weatherResponse.Main.Temp, 1)}°C";
 
-                    labelCity.Text = $"{weatherResponse.Name}";
-                    labelPressure.Text = $"Pressure: {weatherResponse.Main.Pressure} mbar";
-                    labelVisibility.Text = $"Visibility: {Math.Round(weatherResponse.Visibility / 1000, 1)} km";
-                    labelHumidity.Text = $"Humidity: {weatherResponse.Main.Humidity}%";
+                        labelCity.Text = $"{weatherResponse.Name}";
+                        labelPressure.Text = $"Pressure: {weatherResponse.Main.Pressure} mbar";
+                        labelVisibility.Text = $"Visibility: {Math.Round(weatherResponse.Visibility / 1000, 1)} km";
+                        labelHumidity.Text = $"Humidity: {weatherResponse.Main.Humidity}%";
+                    });
                 });
-            });
+            }
+            catch (Exception)
+            {
+                entry1.Text = "";
+                entry1.Placeholder = "Неправильно введен город!";
+            }
         }
     }
 }
